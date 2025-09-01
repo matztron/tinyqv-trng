c928230b0e241d622f8cc762dcbbbddbf1da7056
matztron <muschmatthias@gmail.com>
2025-09-01 23:36:08 +0200

    changed config.json; tests

--- test/test.py
+++ test/test.py
@@ -33,19 +33,30 @@ async def test_project(dut):
     dut._log.info("Test project behavior")
 
     # Test register write and read back
-    await tqv.write_reg(0, 20)
-    assert await tqv.read_reg(0) == 20
+    await tqv.write_reg(0, 0)
+    #assert await tqv.read_reg(0) == 20
+
+    old_rng_val = dut.data_out
 
     # Set an input value, in the example this will be added to the register value
-    dut.ui_in.value = 30
+    #dut.ui_in.value = 30
 
     # Wait for two clock cycles to see the output values, because ui_in is synchronized over two clocks,
     # and a further clock is required for the output to propagate.
-    await ClockCycles(dut.clk, 3)
+    await ClockCycles(dut.clk, 30)
+
+    # value should not have changed as the design is disabled
+    #assert dut.data_out = old_rng_val
+
+    await tqv.write_reg(0, 1)
+
+    await ClockCycles(dut.clk, 30)
+
+    assert dut.data_out != old_rng_val
 
     # The following assertion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    #assert dut.uo_out.value == 50
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
